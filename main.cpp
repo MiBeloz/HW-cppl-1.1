9f6281817b2c2f1aa339649fd3a3a831b06e15e2
MiBeloz <miha_bmv_@mail.ru>
2023-04-26 08:54:54 +0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+void print_vector(const std::vector<int>& my_vector);
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	std::cout << "\tЛямбда-функция\n" << std::endl;
+
+	std::vector<int> my_vector{ 4, 7, 9, 14, 12 };
+
+	std::cout << "Входные данные: ";
+	print_vector(my_vector);
+
+	std::for_each(my_vector.begin(), my_vector.end(), [](auto& value) {if (value % 2) value *= 3; });
+
+	std::cout << "Выходные данные: ";
+	print_vector(my_vector);
+
+	system("pause > nul");
+	return 0;
+}
+
+void print_vector(const std::vector<int>& my_vector) {
+	for (auto const& i : my_vector) {
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+}
